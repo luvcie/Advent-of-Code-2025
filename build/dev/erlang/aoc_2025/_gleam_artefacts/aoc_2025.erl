aa4fcc52b2b4c621c72b05341e7851f07b6b33ac
Luvcie <lucpardo@student.42.fr>
2025-12-03 23:54:31 +0100

    chore: stop tracking the build directory

--- build/dev/erlang/aoc_2025/_gleam_artefacts/aoc_2025.erl
+++
@@ -1,9 +0,0 @@
--module(aoc_2025).
--compile([no_auto_import, nowarn_unused_vars, nowarn_unused_function, nowarn_nomatch, inline]).
--define(FILEPATH, "src/aoc_2025.gleam").
--export([main/0]).
-
--file("src/aoc_2025.gleam", 3).
--spec main() -> nil.
-main() ->
-    playground@helloworld:print_hello().
